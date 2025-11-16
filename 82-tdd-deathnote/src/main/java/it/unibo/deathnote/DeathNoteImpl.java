213d78bd640f61ad0801f4ecef0cab6f8d38874e
Jacopo Girolamini <jacopogirolamini2005@gmail.com>
2025-11-16 16:09:17 +0100

    soluzione esercizio

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/DeathNoteImpl.java
@@ -0,0 +1,111 @@
+package it.unibo.deathnote;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unibo.deathnote.api.DeathNote;
+
+/**
+ * Implementazione di DeathNote usata per l'esercizio TDD.
+ * Questa classe registra nomi, cause e dettagli come richiesto.
+ */
+final class DeathNoteImpl implements DeathNote {
+
+    private final Map<String, DeathData> dNote = new HashMap<>();
+    private long lastWriteTime;
+    private String lastNameWritten;
+
+    @Override
+    public String getRule(final int ruleNumber) {
+        if (ruleNumber <= 0 || ruleNumber > RULES.size()) {
+            throw new IllegalArgumentException("rules index cannot be 0, negative or more than the number of rules");
+        }
+        return RULES.get(ruleNumber - 1);
+    }
+
+    @Override
+    public void writeName(final String name) {
+        if (name == null) {
+            throw new NullPointerException("name parameter shouldn't be null");
+        }
+        if (name.isEmpty()) {
+            return;
+        }
+        lastWriteTime = System.currentTimeMillis();
+        dNote.put(name, new DeathData());
+        lastNameWritten = name;
+    }
+
+    @Override
+    public boolean writeDeathCause(final String cause) {
+        if (cause == null || dNote.isEmpty()) {
+            throw new IllegalStateException("either cause is null or dNote is empty");
+        }
+        //if the cause is written within 40ms 
+        if ((System.currentTimeMillis() - lastWriteTime) <= 40) {
+            dNote.get(lastNameWritten).setDeathCause(cause);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean writeDetails(final String details) {
+        if (details == null || dNote.isEmpty()) {
+            throw new IllegalStateException("cause is null or dNote is empty");
+        }
+        //if the cause is written within 6400ms 
+        if ((System.currentTimeMillis() - lastWriteTime) <= 6400) {
+            dNote.get(lastNameWritten).setDeathDetails(details);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getDeathCause(final String name) {
+        if (!dNote.containsKey(name)) {
+            throw new IllegalArgumentException("name is not in the death note");
+        }
+        return dNote.get(name).getDeathCause();
+    }
+
+    @Override
+    public String getDeathDetails(final String name) {
+        if (!dNote.containsKey(name)) {
+            throw new IllegalArgumentException("name isn't on the death note");
+        }
+        return dNote.get(name).getDeathDetails();
+    }
+
+    @Override
+    public boolean isNameWritten(final String name) {
+        return dNote.containsKey(name);
+    }
+
+    private final class DeathData {
+        private String deathCause;
+        private String deathDetails;
+
+        DeathData() {
+            deathCause = "heart attack";
+            deathDetails = "";
+        }
+
+        public String getDeathCause() {
+            return deathCause;
+        }
+
+        public String getDeathDetails() {
+            return deathDetails;
+        }
+
+        public void setDeathCause(final String cause) {
+            deathCause = cause;
+        }
+
+        public void setDeathDetails(final String details) {
+            deathDetails = details;
+        }
+    }
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,95 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+class TestDeathNote {
+    private final DeathNoteImpl dNote = new DeathNoteImpl();
+
+    @Test
+    void negativeRulesCheck() {
+        try {
+            dNote.getRule(0);
+            dNote.getRule(-1);
+            Assertions.fail("a negative number or 0 as index for .getRule() method should throw the appropriate exception");
+        } catch (final IllegalArgumentException e) {
+            Assertions.assertNotNull(e.getMessage(), "Exception message is null");
+            Assertions.assertNotEquals("", e.getMessage(), "Exception message is empty");
+        }
+    }
+
+    @Test
+    void writeCheck() {
+        Assertions.assertFalse(dNote.isNameWritten("pippo"), "written name: pippo is already in the DeathNote");
+        dNote.writeName("puppi");
+        Assertions.assertTrue(dNote.isNameWritten("puppi"), 
+            "the name do not result as wirtten on the DeathNote even after calling writeName(name)"
+        );
+        Assertions.assertFalse(dNote.isNameWritten("L"), 
+            "a name result as written on the DeathNote even if it wasn't called the writeName() method on it"
+        );
+        Assertions.assertFalse(dNote.isNameWritten(""), 
+            "\"\" result as written in the DeathNote but it shouldn't be"
+        );
+    }
+
+    @Test
+    void deathCauseCheck() throws InterruptedException {
+        try {
+            dNote.writeDeathCause("try cause");
+            Assertions.fail("Writing the cause of death before writing an actual name should throw the appropriate exception");
+        } catch (final IllegalStateException e) {
+            Assertions.assertNotNull(e.getMessage(), "IllegalStateException message is null");
+            Assertions.assertNotEquals("", e.getMessage(), "IllegalStateException message is empty");
+        }
+        dNote.writeName("Masuka");
+        Assertions.assertEquals("heart attack", 
+            dNote.getDeathCause("Masuka"), 
+            "death causa for whom is not specified shoud be \" heart attack \""
+        );
+        dNote.writeName("James Dean");
+        Assertions.assertTrue(dNote.writeDeathCause("karting accident"), 
+            "writing death cause didn't return true but it should have"
+        );
+        Assertions.assertEquals("karting accident", 
+            dNote.getDeathCause("James Dean"), 
+            "expecting \"karting accident\""
+        );
+        Thread.sleep(100);
+        Assertions.assertFalse(dNote.writeDeathCause("assassinated"), 
+            "writing death cause after the time limit should have returned false but it didn't"
+        );
+    }
+
+    @Test
+    void deathDetailsCheck() throws InterruptedException {
+        try {
+            dNote.writeDetails("try details");
+            Assertions.fail("Writing the details before writing an actual name should throw the appropriate exception");
+        } catch (final IllegalStateException e) {
+            Assertions.assertNotNull(e.getMessage(), "IllegalStateException message is null");
+            Assertions.assertNotEquals("", e.getMessage(), "IllegalStateException message is empty");
+        }
+        dNote.writeName("Totò Riina");
+        Assertions.assertEquals(dNote.getDeathDetails("Totò Riina"), 
+            "", 
+            "death details if not specified should be empty \"\" but it isn't"
+        );
+        Assertions.assertTrue(dNote.writeDetails("ran for too long"), 
+            "writing details should have returned true but it didn't"
+        );
+        Assertions.assertEquals("ran for too long", 
+            dNote.getDeathDetails("Totò Riina"), 
+            "expecting \"ran for too long\""
+        );
+        dNote.writeName("Joseph Stalin");
+        Thread.sleep(6400);
+        Assertions.assertFalse(dNote.writeDetails("assassinated"), 
+            "writing death details after the time limit should have returned false but it didn't"
+        );
+        Assertions.assertEquals("", 
+            dNote.getDeathDetails("Joseph Stalin"), 
+            "the method writeDetails returned False correctly but death details are not empty as expected"
+        );
+    }
 }
